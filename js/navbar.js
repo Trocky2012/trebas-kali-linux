6fe281bbdfba6d4f0045ea66812bccfe510b7bf2
Thiago Trolle <thiagotrolle@yahoo.com.br>
2022-11-20 18:27:12 -0400

    Editing navbar v0.3

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,18 @@
-//var path = window.location.pathname.split('/').pop();
-//var path = window.location.pathname
+var path = window.location.pathname.split('/').pop();
 
-//if(path == "index.html"){
+if(path == "index.html"){
 
     document.write(`
     
         <div class="topnav">
-            <a href="index.html">Home</a>
+            <a class="active" href="index.html">Home</a>
             <a href="a-kali-linux.html">Kali linux</a>
             <a href="a-kali-linux-installation.html">Kali linux Installation</a>
             <a href="#test">Test</a>
         </div>
     
     `);
-/*
+
 }
 
 if(path == "a-kali-linux.html"){
@@ -45,4 +44,3 @@ if(path == "a-kali-linux-installation.html"){
     `);
 
 }
-*/
